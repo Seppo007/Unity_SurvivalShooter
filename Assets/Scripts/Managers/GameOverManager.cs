c0497793e4b25aeda1bdb46ef34977247a89639e
Sebastian Schalow <schalowseb@gmail.com>
2018-05-26 23:53:57 +0200

    Set GameOver animation. Play background music. Adjust volumes

--- Assets/Scripts/Managers/GameOverManager.cs
+++ Assets/Scripts/Managers/GameOverManager.cs
@@ -4,16 +4,13 @@ public class GameOverManager : MonoBehaviour
 {
     public PlayerHealth playerHealth;
 
-
     Animator anim;
-
 
     void Awake()
     {
         anim = GetComponent<Animator>();
     }
-
-
+    
     void Update()
     {
         if (playerHealth.currentHealth <= 0)
